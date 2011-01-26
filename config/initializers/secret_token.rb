fb0bce4d32baecbe3597fa355bb0bf454f656de4
batin <batin.oleg@gmail.com>
2011-01-25 19:16:03 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BikesApp::Application.config.secret_token = '9292a23b1b61aa807f71b876da4120525ceccf67af5048f62a06ff3bd893cd3b2ccd5885299f5588caeb26498beb23b63b3a119c78bbff31da9d194814920ef8'
